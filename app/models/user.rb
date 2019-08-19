46c7e4becb85c804034d19ff9fc780aaace4e6c5
peter <leepeter019@gmail.com>
2019-08-18 21:44:03 -0700

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
     attr_accessor :remember_token, :activation_token, :reset_token
 
+    has_many :microposts, dependent: :destroy
+
     before_save :downcase_email
 
     before_create :create_activation_digest
@@ -66,16 +68,20 @@ class User < ApplicationRecord
         reset_sent_at < 2.hours.ago
     end
 
+    def feed
+        Micropost.where("user_id = ?", id)
+    end
+
     private
 
-    def downcase_email
-        self.email = email.downcase
-    end
+        def downcase_email
+            self.email = email.downcase
+        end
 
-    def create_activation_digest
-        self.activation_token = User.new_token
-        self.activation_digest = User.digest(activation_token)
-    end
+        def create_activation_digest
+            self.activation_token = User.new_token
+            self.activation_digest = User.digest(activation_token)
+        end
     
 end
 
